4b74e788286d84c730618133d4329923e80fbb63
aka <aka@junjies-MacBook-Pro.local>
2016-12-30 13:29:05 +0800

    Testing our first attempt at Web API

--- django/tutorial/tutorial/settings.py
+++ django/tutorial/tutorial/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'snippets.EnforceLoginMiddleware.EnforceLoginMiddleware',
 ]
 
 ROOT_URLCONF = 'tutorial.urls'
