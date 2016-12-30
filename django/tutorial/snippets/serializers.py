3b50abfb81dcdb2b3f4c0efa250ace9b588c0f21
aka <aka@junjies-MacBook-Pro.local>
2016-12-30 15:11:48 +0800

    Updating our serializer

--- django/tutorial/snippets/serializers.py
+++ django/tutorial/snippets/serializers.py
@@ -14,3 +14,6 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'snippets')
+
+
+owner = serializers.ReadOnlyField(source='owner.username')
